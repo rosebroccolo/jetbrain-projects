90b151c423d5c6eb4f14185dfa352ca92daaf81d
Rose Broccolo <76491351+rosebroccolo@users.noreply.github.com>
2021-02-01 10:44:24 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+//concatenation
+const name = 'Rose'
+const age = 30;
+console.log('My name is' + name + 'and I am ' +  age);
+
+//template string - use back ticks
+console.log(`My name is ${name} and I am ${age}`);
+
+//method is a function associated with an object
+
+let s;
+s = 'technology, computers, IT, code';
+console.log (s.split(','));
+
+//Arrays = variables that hold multiple values
+
+
+
+//loops
+
+
+
+
+
+
+
+
+
+
+
